6494fed40d0d87042a329e5407491df905fc4266
SowmyaDondeti <sowmyar987@gmail.com>
2019-08-20 18:22:42 -0500

    new data added

---
+++ src/main/resources/db/migration/V4__update_data.sql
@@ -0,0 +1,37 @@
+alter table menu
+
+    add category varchar;
+
+
+
+    update menu set category = 'Biryani & Rice'
+
+    where name = 'Paneer Biryani :';
+
+    update menu set category = 'Biryani & Rice'
+
+    where name = 'Egg Biryani :';
+
+    update menu set category = 'Biryani & Rice'
+
+    where name = 'Chicken Biryani :';
+
+    update menu set category = 'Biryani & Rice'
+
+    where name = 'Boneless Chicken Biryani :';
+
+    update menu set category = 'Biryani & Rice'
+
+    where name = 'Vegetable Biryani :';
+
+    update menu set category = 'Biryani & Rice'
+
+    where name = 'Goat Biryani :';
+
+    update menu set category = 'Biryani & Rice'
+
+    where name = 'Shrimp Biryani :';
+
+    update menu set category = 'Biryani & Rice'
+
+    where name = 'Goat Keema Biryani :';
