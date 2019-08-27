5b5c78a957a6d804d9005cb386a61282ac41b298
SowmyaDondeti <sowmyar987@gmail.com>
2019-08-27 16:13:21 -0500

    Updated sql files

--- src/main/resources/db/migration/V1__initial_db.sql
+++ src/main/resources/db/migration/V1__initial_db.sql
@@ -2,5 +2,9 @@ create table menu (
       id serial primary key,
       name varchar not null,
       description varchar not null,
-      price numeric not null
+      price numeric not null,
+      picture_url varchar not null,
+      category varchar not null,
+      type varchar not null,
+      spice_level varchar not null
     );
--- src/main/resources/db/migration/V2__initial_data.sql
+++ src/main/resources/db/migration/V2__initial_data.sql
@@ -1,15 +1,64 @@
-insert into menu (name, description, price) values ('Paneer Biryani :', 'Indian cottage cheese flavored with onions, herbs, spices, and slow cooked with basmati rice.', 11.00);
+insert into menu (name, description, price,picture_url,category,type,spice_level)
+values ('Paneer Biryani', 'Indian cottage cheese flavored with onions, herbs, spices, and slow cooked with basmati rice.', 11.00,
+'https://4.bp.blogspot.com/-oYfZwRbuEeQ/WthWbOie_3I/AAAAAAAArJc/w_vmwBeQbPk6ezfQ6yTfC9FnMborN2RSwCLcBGAs/s640/IMG_7407.JPG',
+'Biryani & Rice','Veg','Medium');
 
-    insert into menu (name, description, price) values ('Egg Biryani :', 'Egg flavored with herbs, spices, and slow cooked with basmati rice.', 14.00);
+insert into menu (name, description, price,picture_url,category,type,spice_level)
+values ('Egg Biryani', 'Egg flavored with herbs, spices, and slow cooked with basmati rice.', 14.00,
+'https://6d1bdf0e0edb084eae10-5bfabe5484726969ac662c6d377e2f3c.ssl.cf2.rackcdn.com/uploads/14031/7b26f5505d8779f0d9d6a75ae1f5f10d.JPG',
+'Biryani & Rice','Non-Veg','Medium');
 
-    insert into menu (name, description, price) values ('Chicken Biryani :', 'Bone-in chicken flavored with onions, herbs, spices, and slow cooked with basmati rice.', 13.00);
+insert into menu (name, description, price,picture_url,category,type,spice_level)
+values ('Chicken Biryani', 'Bone-in chicken flavored with onions, herbs, spices, and slow cooked with basmati rice.', 13.00,
+'https://www.thedeliciouscrescent.com/wp-content/uploads/2016/05/Easy-Hyderabadi-Chicken-Biryani.jpg','Biryani & Rice',
+'Non-Veg','High');
 
-    insert into menu (name, description, price) values ('Boneless Chicken Biryani :', 'Boneless chicken flavored with onions, herbs, spices, and slow cooked with basmati rice.', 12.00);
+insert into menu (name, description, price,picture_url,category,type,spice_level)
+values ('Boneless Chicken Biryani', 'Boneless chicken flavored with onions, herbs, spices, and slow cooked with basmati rice.', 12.00,
+'http://irepo.primecp.com/2016/03/258905/recipe-4440_Large500_ID-1451929.jpg?v=1451929','Biryani & Rice','Non-Veg',
+'Medium');
 
-    insert into menu (name, description, price) values ('Vegetable Biryani :', 'Vegetables flavored with onions, herbs, spices, and slow cooked with basmati rice.', 13.00);
+insert into menu (name, description, price,picture_url,category,type,spice_level)
+values ('Vegetable Biryani', 'Vegetables flavored with onions, herbs, spices, and slow cooked with basmati rice.', 13.00,
+'https://www.tarladalal.com/members/9306/big/big_vegetable_biryani_(__desi_khana)-8943.jpg?size=696X905','Biryani & Rice','Veg','Low');
 
-    insert into menu (name, description, price) values ('Goat Biryani :', 'Bone-in goat flavored with onions, herbs, spices, and slow cooked with basmati rice.', 15.00);
+insert into menu (name, description, price,picture_url,category,type,spice_level)
+values ('Goat Biryani', 'Bone-in goat flavored with onions, herbs, spices, and slow cooked with basmati rice.', 15.00,
+'https://www.whiskaffair.com/wp-content/uploads/2018/02/Hyderabadi-Mutton-Biryani-6.jpg','Biryani & Rice','Non-Veg','High');
 
-    insert into menu (name, description, price) values ('Shrimp Biryani :', 'Shrimp flavored with onions, herbs, spices, and slow cooked with basmati rice.', 14.00);
+insert into menu (name, description, price,picture_url,category,type,spice_level)
+values ('Shrimp Biryani', 'Shrimp flavored with onions, herbs, spices, and slow cooked with basmati rice.', 14.00,
+'https://ministryofcurry.com/wp-content/uploads/2017/06/Shrimp-Biryani-1-1-500x500.jpg','Biryani & Rice','Non-Veg','Medium');
 
-    insert into menu (name, description, price) values ('Goat Keema Biryani :', 'Ground goat flavored with onions, herbs, spices, and slow cooked with basmati rice.', 16.00);
+insert into menu (name, description, price,picture_url,category,type,spice_level)
+values ('Goat Keema Biryani', 'Ground goat flavored with onions, herbs, spices, and slow cooked with basmati rice.', 16.00,
+'https://i.ytimg.com/vi/aMDtgimuSqs/maxresdefault.jpg','Biryani & Rice','Non-Veg','Medium');
+
+insert into menu (name, description, price,picture_url,category,type,spice_level)
+values ('Goat Fry Biryani - Weekend Only', 'Fried goat flavored with onions, herbs, spices, and slow cooked with basmati rice.', 16.00,
+'https://foodalltime.com/wp-content/uploads/2018/07/Hyderabadi-Mutton-Dum-Biryani-Recipe-2F-Hyderabadi-Dum-Biryani-Recipe-1.jpg','Biryani & Rice','Non-Veg','Medium');
+
+insert into menu (name, description, price,picture_url,category,type,spice_level)
+values ('Ulavacharu Chicken Biryani', 'Chicken flavored with horse gram soup, herbs, spices, and slow cooked with basmati rice.', 15.00,
+'https://ehc-west-0-bucket.s3.us-west-2.amazonaws.com/web/documents/hhcolumbus/menu_item_images/b4d8551f-d099-4b08-9f22-11c6245c80c2.jpg?5d503ebe7ba9d','Biryani & Rice',
+'Non-Veg','Medium');
+
+insert into menu (name, description, price,picture_url,category,type,spice_level)
+values ('Ulavacharu Goat Biryani', 'Goat flavored with horse gram soup, herbs, spices, and slow cooked with basmati rice.', 16.00,
+'https://i.ytimg.com/vi/cqpleknKcHw/maxresdefault.jpg','Biryani & Rice','Non-Veg','Medium');
+
+insert into menu (name, description, price,picture_url,category,type,spice_level)
+values ('White Rice', '', 3.00,'https://hips.hearstapps.com/vidthumb/images/delish-u-rice-2-1529079587.jpg','Biryani & Rice',
+'Vegan','Low');
+
+insert into menu (name, description, price,picture_url,category,type,spice_level)
+values ('Jeera Rice', 'Rice cooked with cumin seeds.', 7.00,
+'https://myfoodstory.com/wp-content/uploads/2018/07/Perfect-Jeera-Rice-Indian-Cumin-Rice-4.jpg','Biryani & Rice','Veg','Low');
+
+insert into menu (name, description, price,picture_url,category,type,spice_level)
+values ('Bisi Bele Bath', 'Translates to "hot lentil rice".', 9.00,
+'https://i.pinimg.com/originals/19/81/19/1981197b89d8e7ecbd68c0080c24a6b5.jpg','Biryani & Rice','Veg','Medium');
+
+insert into menu (name, description, price,picture_url,category,type,spice_level)
+values ('Curd Rice', 'Rice with yogurt, herbs, and spices.', 9.00,
+'https://www.cookwithmanali.com/wp-content/uploads/2015/01/Curd-Rice-South-Indian.jpg','Biryani & Rice','Veg','Low');
